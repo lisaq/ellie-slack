36fbbcd2e088b033ee46b48a68878b7c8b5dcd5b
Lisa Quatmann <lisa.quatmann@correctiv.org>
2018-03-15 12:05:54 +0100

    heroku env vars

--- rtmbot.py
+++ rtmbot.py
@@ -165,7 +165,8 @@ if __name__ == "__main__":
 
     config = yaml.load(file('rtmbot.conf', 'r'))
     debug = config["DEBUG"]
-    bot = RtmBot(os.getenv("SLACK_TOKEN", config["SLACK_TOKEN"]))
+    slack_token = os.getenv('SLACK_TOKEN', config['SLACK_TOKEN'])
+    bot = RtmBot(slack_token)
     site_plugins = []
     files_currently_downloading = []
     job_hash = {}
