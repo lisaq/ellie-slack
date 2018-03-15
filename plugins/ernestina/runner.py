08ac0d876e375e183671b5b14308bbe6ff23b05b
Lisa Quatmann <lisa.quatmann@correctiv.org>
2018-03-15 11:01:38 +0100

    modifying so that ernestina only responds to dms or mentions

--- plugins/ernestina/runner.py
+++ plugins/ernestina/runner.py
@@ -1,14 +1,21 @@
+import os
 import random
 import time
 
 import ernestina
 
+import yaml
+
+
 crontable = []
 outputs = []
+config = yaml.load(file('rtmbot.conf', 'r'))
+bot_id = os.getenv("BOT_SLACK_ID", config["BOT_SLACK_ID"])
 
 
 def process_message(data):
-    # Sleep for a bit before replying; you'll seem more real this way
-    time.sleep(random.randint(0, 9) * .2)
-    outputs.append(
-        [data['channel'], "{}".format(ernestina.respond(data['text']))])
+    if bot_id in data['text'] or data['channel'].startswith('D'):
+        # Sleep for a bit before replying; you'll seem more real this way
+        time.sleep(random.randint(0, 9) * .2)
+        outputs.append(
+            [data['channel'], "{}".format(ernestina.respond(data['text']))])
